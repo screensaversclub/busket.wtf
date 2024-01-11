339c0a39e8110aa59bed6ec2bdab24ddbc4aad40
Siah <siah@goodwork.sg>
2024-01-12 01:37:18 +0800

    feat: logo goes to home

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -3,14 +3,14 @@
 	const currentPath = $page.url.pathname;
 </script>
 
-<nav><figure>🚌</figure></nav>
+<nav><a href="/"><figure>🚌</figure></a></nav>
 
 <style lang="postcss">
 	nav {
-		@apply bg-[transparent] flex gap-4 items-center justify-center p-2 border-b border-b-yellow-400 bg-yellow-200 h-12 flex-grow-0 flex-shrink-0;
+		@apply bg-[transparent] flex gap-4 items-center justify-center p-2 border-b border-b-yellow-400 bg-yellow-200 h-16 flex-grow-0 flex-shrink-0;
 
 		figure {
-			@apply text-2xl;
+			@apply text-3xl;
 		}
 	}
 </style>
