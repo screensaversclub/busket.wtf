27c51ca79fc214c1ce61cbf8423d176e6b4c1203
Siah <siah@goodwork.sg>
2024-01-13 10:54:50 +0800

    fix: storage bug

--- src/lib/favs-store.ts
+++ src/lib/favs-store.ts
@@ -2,9 +2,11 @@ import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 
 const favsStore = !browser ? '' : localStorage.getItem('busket-favs');
-export const favs = writable(favsStore !== null ? favsStore.split(';') : []);
+export const favs = writable(
+	favsStore !== null ? favsStore.split(';').filter((a) => a.length > 0) : []
+);
 favs.subscribe((v) => {
 	if (browser) {
-		localStorage.setItem('busket-favs', v.join(';'));
+		localStorage.setItem('busket-favs', v.filter((a) => a.length > 0).join(';'));
 	}
 });
