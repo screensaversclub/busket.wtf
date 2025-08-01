c1c7143878510cf6119b8c0eb003bc77b785da0f
Siah <siah@goodwork.sg>
2025-08-01 22:12:44 +0800

    fix: update endpoints

--- src/lib/lta-endpoints.ts
+++ src/lib/lta-endpoints.ts
@@ -1,5 +1,5 @@
 export const DATAMALL_BUS_ARRIVAL_ENDPOINT =
-	'http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2';
+	'https://datamall2.mytransport.sg/ltaodataservice/v3/BusArrival';
 
 export const DATAMALL_BUS_STOPS_ENDPOINT =
-	'http://datamall2.mytransport.sg/ltaodataservice/BusStops';
+	'https://datamall2.mytransport.sg/ltaodataservice/BusStops';
